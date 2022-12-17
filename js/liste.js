0743026914b30b67d59d0412eb3586fb142e7d06
Kyosuke <fire.gaming95800@gmail.com>
2022-12-17 11:05:02 +0100

    my first commit

---
+++ js/liste.js
@@ -0,0 +1,8 @@
+const download = document.querySelector(".download-here");
+
+download.addEventListener("click", () => {
+  window.scrollTo({
+    top: 10000,
+    behavior: "smooth",
+  });
+});
---
+++ js/script.js
@@ -0,0 +1,16 @@
+const nav_home = document.querySelector(".nav-home1");
+const nav_liste = document.querySelector(".nav-liste1");
+const nav_venezuela = document.querySelector(".nav-venezuela1");
+
+const TL1 = new TimelineMax({ paused: true });
+
+TL1.from(nav_home, 1.3, { opacity: 0 }, "+=0.3")
+  .from(nav_liste, 1.3, { opacity: 0 }, "-=0.75")
+  .from(nav_venezuela, 1.3, { opacity: 0 }, "-=0.75");
+
+TL1.play();
+
+alert("✅ Vous vous trouvez bien sur le site de Xavier et Baptiste !");
+alert(
+  "⚠️ Vous pouvez écouter de la belle musique à l'aide de la source audio se situant à côté des autres pages !"
+);
